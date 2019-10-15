7b7d557374a8a29ced7494a14bfd1e866d83fd6d
Cx12 <56474039+Sixmusic807725@users.noreply.github.com>
2019-10-16 06:22:39 +0700

    Create Secret2.sh

---
+++ Secret2.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+#Apa Lu Mau Recode? Ini Buat Ddos Tolol
+#Dasar Anak Ngentod Yang Taunya Recode Aja:v
+#Rapi Kan Kodingan Gw Su!?!?
+
+clear
+r='\033[31;2m' #merah
+g='\033[32;1m' #hijau
+b='\033[34;2m' #biru
+p='\033[35;1m' #purple
+cy='\033[36;2m' #cyan
+w='\033[37;1m' #putih
+y='\033[33;1m' #kuning
+bl='\33[36;1m' #BlueLight
+echo $bl""
+figlet -f standard Pack Ddos
+echo "_____________________________________________________________"
+
+echo "Tools Secret On Termosss" | lolcat
+echo $g"Author : jokowi" 
+echo "Instagram : jokowi" | lolcat
+echo $g"Recode? izin dulu ke jokowi!!"
+echo "tools ini sudah saya mod atau ssya modif"
+echo "Mau tau pasword nya,? pw:IndonesiaHancur"
+echo "Mau tau Usr nya,? usr:Republik"
+echo "pw nya itu bukan menghina Indonesia"
+echo "tetapi emang sekarang rakyat Indonesia sedang war"
+echo "dengan DPR (Dewan Perwakilan Rakyat)"
+ echo "_____________________________________________________________"
+
+echo $bl"Login Dulu Biar Aman:V"
+echo ""
+
+sleep 3
+
+read -p "Username : " usr
+
+if [ $usr = "Republik" ]
+then
+
+figlet -f standard Sukses! | lolcat
+
+else
+  figlet -f slant Gagal! | lolcat
+
+sleep 2
+
+sh Secret2.sh
+
+fi
+
+read -p "Password: " pwd
+
+if [ $pwd = "IndonesiaHancur" ]
+then
+
+   figlet -f standard Sukses! | lolcat
+
+sleep 1
+
+else
+
+    figlet -f slant Gagal! | lolcat
+
+sleep 2
+
+sh Secret2.sh
+
+fi
+
+echo $b "Selamat anda Menggunakan Tools Ini!! :)"
+
+sleep 1
+
+clear
+
+#Process Tools
+
+sh Secret1.sh
